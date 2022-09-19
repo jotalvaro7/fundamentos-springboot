b98aae9f6f20b5d4b2e5dce51115e9587f9fe770
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-09-19 12:45:09 -0500

    Utilizando Logs

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,8 +4,8 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,30 +16,40 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class FundamentosApplication implements CommandLineRunner {
 
 
-	private ComponentDependency componentDependency;
-	private MyBean myBean;
-	private MyBeanWithDependency myBeanWithDependency;
-	private User user;
+    //LLamando logs
+    private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
-	public FundamentosApplication( ComponentDependency componentDependency, MyBean myBean,
-								   MyBeanWithDependency myBeanWithDependency, User user ) {
-		this.componentDependency = componentDependency;
-		this.myBean = myBean;
-		this.myBeanWithDependency = myBeanWithDependency;
-		this.user = user;
-	}
+    private ComponentDependency componentDependency;
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+    private User user;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundamentosApplication.class, args);
+    public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean,
+                                  MyBeanWithDependency myBeanWithDependency, User user) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+        this.user = user;
+    }
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosApplication.class, args);
 
-	@Override
-	public void run(String... args)  {
-		componentDependency.saludar();
-		myBean.print();
-		myBeanWithDependency.printWithDependency();
-		System.out.println(user.nombreCompleto());
+    }
 
-	}
+    @Override
+    public void run(String... args) {
+        componentDependency.saludar();
+        myBean.print();
+        myBeanWithDependency.printWithDependency();
+        System.out.println(user.nombreCompleto());
+        try {
+			//Codigo
+            int value = 10/0;
+            LOGGER.info("Mi valor : " + value);
+        } catch (Exception e) {
+            LOGGER.error("Esto es un error al dividir por cero" + e.getMessage());
+        }
+
+    }
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImpl.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImpl.java
@@ -1,8 +1,13 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class MyBeanWithDependencyImpl implements MyBeanWithDependency {
 
     private MyOperation myOperation;
+
+    private final Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImpl.class);
 
     public MyBeanWithDependencyImpl(MyOperation myOperation) {
         this.myOperation = myOperation;
@@ -10,7 +15,9 @@ public class MyBeanWithDependencyImpl implements MyBeanWithDependency {
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printWithDependency");
         int numero = 1;
+        LOGGER.debug("El numero enviado como parametro a la dependencia operation es : " + numero);
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementacion de un bean con dependencia");
     }
